0cc317b01e1bc78ce51c9adeaae1ff415e4886f5
aungkhantkyaw <aungkhant.kyaw@brycenmyanmar.com.mm>
2022-07-05 14:26:08 +0630

    Create color swatch generator

---
+++ pages/api/generate-color-swatch.tsx
@@ -0,0 +1,122 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ColorFormat = {
+  type: string;
+  color_code: Array<number>;
+};
+
+type Error = {
+  error_message: string;
+};
+
+type GeneratedColor = {
+  color: Array<ColorFormat>;
+};
+
+type QueryData = {
+  type?: string;
+  max_range?: number;
+  byte_range?: number;
+};
+
+export default function generateRandomColor(
+  req: NextApiRequest,
+  res: NextApiResponse<GeneratedColor | Error>
+) {
+  const defaultColorFormat = ["rgb", "hsl"];
+  let result: Array<number>;
+  let colorSwatch: Array<ColorFormat> = [];
+
+  const queryData: QueryData = req.query;
+
+  if (queryData.type) {
+    if (queryData.type === "") {
+      res.status(400).json({
+        error_message: "Please provide valid color type",
+      });
+    } else if (!queryData.max_range || isNaN(queryData.max_range)) {
+      res.status(400).json({
+        error_message: "Max range is required for custom color space",
+      });
+    } else if (queryData.max_range < 1) {
+      res.status(400).json({
+        error_message: "Max range value must be greater than 0",
+      });
+    } else if (!queryData.byte_range || isNaN(queryData.byte_range)) {
+      res.status(400).json({
+        error_message: "Byte range is required for custom color space",
+      });
+    } else if (queryData.byte_range < 3) {
+      res.status(400).json({
+        error_message: "Byte range must be at least 3 bytes",
+      });
+    } else {
+      for (let i = 0; i < 5; i++) {
+        result = customRandomColorGenerator(
+          queryData.max_range,
+          queryData.byte_range
+        );
+
+        colorSwatch.push({
+          type: queryData.type,
+          color_code: result,
+        });
+      }
+
+      res.status(200).json({
+        color: colorSwatch,
+      });
+    }
+  } else {
+    for (let i = 0; i < 5; i++) {
+      let randomFormat: string =
+        defaultColorFormat[Math.floor(Math.random() * 2)];
+      result = defaultRandomColorGenerator(randomFormat);
+
+      colorSwatch.push({
+        type: randomFormat,
+        color_code: result,
+      });
+    }
+
+    res.status(200).json({
+      color: colorSwatch,
+    });
+  }
+}
+
+/**
+ * Generate default random color swatch if no value provide
+ * by client side
+ * @param colorFormat color representation (rgb, hls, etc...)
+ */
+function defaultRandomColorGenerator(colorFormat: string) {
+  let colorCode: Array<number> = [];
+
+  if (colorFormat === "rgb") {
+    colorCode = [randomInteger(255), randomInteger(255), randomInteger(255)];
+  } else if (colorFormat === "hsl") {
+    colorCode = [randomInteger(360), randomInteger(100), randomInteger(100)];
+  }
+
+  return colorCode;
+}
+
+/**
+ * Generate random color swatch with custom color space
+ * by client side
+ * @param max maximum range for custom color space
+ */
+function customRandomColorGenerator(max: number, byteRange: number) {
+  let colorCode: Array<number> = [];
+
+  for (let i = 0; i < byteRange; i++) {
+    colorCode.push(randomInteger(max));
+  }
+
+  return colorCode;
+}
+
+function randomInteger(max: number) {
+  return Math.floor(Math.random() * (max + 1));
+}
